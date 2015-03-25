73d41c4fa79ea59195e701cc270bd098802baf04
Wolfgang Herget <wolfgang.herget@dfki.de>
2015-03-25 15:19:00 +0100

    Fix typo in license header.

--- compass-rest/src/main/java/de/dfki/asr/compass/rest/providers/JacksonContextResolver.java
+++ compass-rest/src/main/java/de/dfki/asr/compass/rest/providers/JacksonContextResolver.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of COMPASS. It is subject to the license terms in
  * the LICENSE file found in the top-level directory of this distribution.
- * (Also avialable at http://www.apache.org/licenses/LICENSE-2.0.txt)
+ * (Also available at http://www.apache.org/licenses/LICENSE-2.0.txt)
  * You may not use this file except in compliance with the License.
  */
 package de.dfki.asr.compass.rest.providers;
