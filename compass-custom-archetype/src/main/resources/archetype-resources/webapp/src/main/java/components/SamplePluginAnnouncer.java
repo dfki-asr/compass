210caf20936f351238c34bce0e4b3bb361a60adc
Wolfgang Herget <wolfgang.herget@dfki.de>
2015-03-27 14:23:57 +0100

    PluginAnnouncer doesn't need to be @Stateless

--- compass-custom-archetype/src/main/resources/archetype-resources/webapp/src/main/java/components/SamplePluginAnnouncer.java
+++ compass-custom-archetype/src/main/resources/archetype-resources/webapp/src/main/java/components/SamplePluginAnnouncer.java
@@ -5,9 +5,7 @@ package ${package}.components;
 
 import de.dfki.asr.compass.web.plugins.PluginViewRegistry;
 import de.dfki.asr.compass.web.plugins.api.PluginAnnouncer;
-import javax.ejb.Stateless;
 
-@Stateless
 public class SamplePluginAnnouncer implements PluginAnnouncer {
 	@Override
 	public void announcePlugin(PluginViewRegistry registry) {
