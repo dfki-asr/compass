a2648cf0d908c5f4c4ad81b4666d441da7838ede
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-04-01 16:39:51 +0200

    Fix SampleRest

--- compass-custom-archetype/src/main/resources/archetype-resources/rest/src/main/java/rest/SampleRESTService.java
+++ compass-custom-archetype/src/main/resources/archetype-resources/rest/src/main/java/rest/SampleRESTService.java
@@ -1,17 +1,17 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package ${package}.components;
+package ${package}.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 @Path("/sample")
+@javax.ejb.Stateless
 public class SampleRESTService {
 
 	@GET
-	@Path("/sampleREST")
 	public Response getSampleModelList() {
 		return Response.status(Response.Status.NOT_FOUND).build();
 	}
