8583baf9e50450682a32f67018a6e4c71d4b79ff
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-04-01 14:25:55 +0200

    Fix SampleRestService

--- compass-custom-archetype/src/main/resources/archetype-resources/rest/src/main/java/rest/SampleRESTService.java
+++ compass-custom-archetype/src/main/resources/archetype-resources/rest/src/main/java/rest/SampleRESTService.java
@@ -8,12 +8,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 @Path("/sample")
-@javax.ejb.Stateless
 public class SampleRESTService {
 
 	@GET
 	@Path("/sampleREST")
 	public Response getSampleModelList() {
-		Response.status(Response.Status.NOT_FOUND).build();
+		return Response.status(Response.Status.NOT_FOUND).build();
 	}
 }
