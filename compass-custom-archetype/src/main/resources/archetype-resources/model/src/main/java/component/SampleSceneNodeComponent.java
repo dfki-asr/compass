20d415d38b3eef913536b78ee9f131fc8252758e
Wolfgang Herget <wolfgang.herget@dfki.de>
2015-04-07 17:09:53 +0200

    Add documentation to SampleSceneNodeComponent

--- compass-custom-archetype/src/main/resources/archetype-resources/model/src/main/java/component/SampleSceneNodeComponent.java
+++ compass-custom-archetype/src/main/resources/archetype-resources/model/src/main/java/component/SampleSceneNodeComponent.java
@@ -12,6 +12,17 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * The Entity (database storage) class for the sample component.
+ * Each SceneNodeComponent you develop needs to have its data stored using JPA,
+ * which is why it (at the very minimum) needs the @Entity, as well as the
+ * @JsonSubTypes annotation. The JAXB annotations are used mainly for loading
+ * sample data at startup, see COMPASS' core "database-initialization" module.
+ * Sometimes, you need more jackson annotations (@Json...) to customize serialization.
+ * If you need classes to support this, e.g. @JsonSerialize(using=Custom.class),
+ * these are better suited to live in the REST module. See the COMPASS core
+ * REST module on how to perform this (look for "Mixin").
+ */
 @Entity
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
