97154141a28ce2f25b0f3dd696b3b8d1c6c49d72
Wolfgang Herget <wolfgang.herget@dfki.de>
2015-04-07 17:10:19 +0200

    Add Documentation to SampleRESTService

--- compass-custom-archetype/src/main/resources/archetype-resources/rest/src/main/java/rest/SampleRESTService.java
+++ compass-custom-archetype/src/main/resources/archetype-resources/rest/src/main/java/rest/SampleRESTService.java
@@ -7,6 +7,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+/**
+ * An additional REST service.
+ * You can, of course, extend COMPASS' REST API by adding more JAX-RS classes,
+ * such as this.
+ * You can also add functionality (to a certain extent) to the existing API,
+ * by specifying identical @Path annotations to something that is already in
+ * COMPASS' core REST classes. You are limited by JAX-RS' method selection
+ * algorithm however, which only looks for @Path, Method Type (@GET) and
+ * Content-Type (@Consumes, @Produces) annotations. See the JAX-RS standard for
+ * details.
+ * Consider providing a REST API for your Business Logic methods.
+ */
 @Path("/sample")
 @javax.ejb.Stateless
 public class SampleRESTService {
