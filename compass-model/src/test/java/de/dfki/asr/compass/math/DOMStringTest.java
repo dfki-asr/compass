67e28c73fdf13e72db443fe2c2f4f98fae654f87
Wolfgang Herget <wolfgang.herget@dfki.de>
2015-04-10 13:29:39 +0200

    Add Test for DOMString conversions

---
+++ compass-model/src/test/java/de/dfki/asr/compass/math/DOMStringTest.java
@@ -0,0 +1,31 @@
+package de.dfki.asr.compass.math;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class DOMStringTest {
+	@Test
+	public void shouldConvertUnitVector() {
+		Vector3f vector = new Vector3f(0, 1, 0);
+		assertEquals(vector.toDOMString(), "0.0 1.0 0.0");
+	}
+
+	@Test
+	public void shouldMaintainOrder() {
+		Vector3f vector = new Vector3f(1.0f, 2.0f, 3.0f);
+		assertEquals(vector.toDOMString(), "1.0 2.0 3.0");
+	}
+
+	@Test
+	public void shouldParseZeroVector() {
+		Vector3f vector = Vector3f.fromDOMString("0.0 0.0 0.0");
+		assertEquals(vector.x, 0.0f);
+		assertEquals(vector.y, 0.0f);
+		assertEquals(vector.z, 0.0f);
+	}
+
+	@Test(expectedExceptions = {NumberFormatException.class})
+	public void shouldThrowOnTooFewEntries() {
+		Vector3f vector = Vector3f.fromDOMString("0.0 0.0");
+	}
+}
