98d664caf8e9e9a20b0f7eb537ea2655b9c6caa9
Wolfgang Herget <wolfgang.herget@dfki.de>
2015-04-10 13:42:30 +0200

    Add missing license.

--- compass-model/src/test/java/de/dfki/asr/compass/math/Vector3fDOMStringTest.java
+++ compass-model/src/test/java/de/dfki/asr/compass/math/Vector3fDOMStringTest.java
@@ -1,3 +1,9 @@
+/*
+ * This file is part of COMPASS. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this distribution.
+ * (Also available at http://www.apache.org/licenses/LICENSE-2.0.txt)
+ * You may not use this file except in compliance with the License.
+ */
 package de.dfki.asr.compass.math;
 
 import org.testng.annotations.Test;
