eee6e353bc7847132b9c94b4189002c51f960100
Wolfgang Herget <wolfgang.herget@dfki.de>
2015-04-23 17:33:19 +0200

    OrientationAngleTest: spell out the test cases.

--- compass-model/src/test/java/de/dfki/asr/compass/math/OrientationAngleTest.java
+++ compass-model/src/test/java/de/dfki/asr/compass/math/OrientationAngleTest.java
@@ -8,32 +8,163 @@ package de.dfki.asr.compass.math;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.closeTo;
+import static de.dfki.asr.compass.test.matcher.EulerAngleSimilarity.similarTo;
 import org.testng.annotations.Test;
 
+@SuppressWarnings("PMD.ExcessivePublicCount")
 public class OrientationAngleTest {
-	private static final float EQUALS_DELTA = 0.0001f;
-
 	@Test
-	public void RetainEulerAngles() {
-		double[] steps = {0, 90, 180, -90, -180};
-		for (double yaw : steps) {
-			for (double pitch : steps) {
-				for (double roll : steps) {
-						sameEulerAngles(yaw, pitch, roll);
-				}
-			}
-		}
+	public void yaw0Pitch0Roll0() {
+		sameEulerAngles(0, 0, 0);
 	}
 
-	private void sameEulerAngles(final double yaw, final double pitch, final double roll) {
+	@Test
+	public void yaw90Pitch0Roll0() {
+		sameEulerAngles(90, 0, 0);
+	}
+
+	@Test
+	public void yaw180Pitch0Roll0() {
+		sameEulerAngles(180, 0, 0);
+	}
+
+	@Test
+	public void yawMinus90Pitch0Roll0() {
+		sameEulerAngles(-90, 0, 0);
+	}
+
+	@Test
+	public void yaw0Pitch90Roll0() {
+		sameEulerAngles(0, 90, 0);
+	}
+
+	@Test
+	public void yaw90Pitch90Roll0() {
+		sameEulerAngles(90, 90, 0);
+	}
+
+	@Test
+	public void yaw180Pitch90Roll0() {
+		sameEulerAngles(180, 90, 0);
+	}
+
+	@Test
+	public void yawMinus90Pitch90Roll0() {
+		sameEulerAngles(-90, 90, 0);
+	}
+
+
+	@Test
+	public void yaw0PitchMinus90Roll0() {
+		sameEulerAngles(0, -90, 0);
+	}
+
+	@Test
+	public void yaw90PitchMinus90Roll0() {
+		sameEulerAngles(90, -90, 0);
+	}
+
+	@Test
+	public void yaw180PitchMinus90Roll0() {
+		sameEulerAngles(180, -90, 0);
+	}
+
+	@Test
+	public void yawMinus90PitchMinus90Roll0() {
+		sameEulerAngles(-90, -90, 0);
+	}
+
+	@Test
+	public void yaw0Pitch0Roll90() {
+		sameEulerAngles(0, 0, 90);
+	}
+
+	@Test
+	public void yaw90Pitch0Roll90() {
+		sameEulerAngles(90, 0, 90);
+	}
+
+	@Test
+	public void yaw180Pitch0Roll90() {
+		sameEulerAngles(180, 0, 90);
+	}
+
+	@Test
+	public void yawMinus90Pitch0Roll90() {
+		sameEulerAngles(-90, 0, 90);
+	}
+
+	@Test
+	public void yaw0Pitch0Roll180() {
+		sameEulerAngles(0, 0, 180);
+	}
+
+	@Test
+	public void yaw90Pitch0Roll180() {
+		sameEulerAngles(90, 0, 180);
+	}
+
+	@Test
+	public void yaw180Pitch0Roll180() {
+		sameEulerAngles(180, 0, 180);
+	}
+
+	@Test
+	public void yawMinus90Pitch0Roll180() {
+		sameEulerAngles(-90, 0, 180);
+	}
+
+	@Test
+	public void yaw0Pitch0RollMinus90() {
+		sameEulerAngles(0, 0, -90);
+	}
+
+	@Test
+	public void yaw90Pitch0RollMinus90() {
+		sameEulerAngles(90, 0, -90);
+	}
+
+	@Test
+	public void yaw180Pitch0RollMinus90() {
+		sameEulerAngles(180, 0, -90);
+	}
+
+	@Test
+	public void yawMinus90Pitch0RollMinus90() {
+		sameEulerAngles(-90, 0, -90);
+	}
+
+	// The following tests are not from the above SO(3) sample set:
+	// (and therefore, promptly, don't work.)
+
+//	@Test
+//	public void yaw0Pitch90Roll90() {
+//		sameEulerAngles(0, 90, 90);
+//	}
+//
+//	@Test
+//	public void yaw0Pitch180RollMinus0() {
+//		sameEulerAngles(0, 180, 0);
+//	}
+
+//	@Test
+//	public void testAllCombinations() {
+//		float[] steps = {0, 90, 180, -90, -180};
+//		for (float yaw : steps) {
+//			for (float pitch : steps) {
+//				for (float roll : steps) {
+//					sameEulerAngles(yaw, pitch, roll);
+//				}
+//			}
+//		}
+//	}
+
+	private void sameEulerAngles(final float yaw, final float pitch, final float roll) {
 		Orientation o = new Orientation();
 		o.setLocalYaw(yaw);
 		o.setLocalPitch(pitch);
 		o.setLocalRoll(roll);
-		assertThat("yaw",   o.getLocalYaw(),   is(closeTo(yaw, EQUALS_DELTA)));
-		assertThat("pitch", o.getLocalPitch(), is(closeTo(pitch, EQUALS_DELTA)));
-		assertThat("roll",  o.getLocalRoll(),  is(closeTo(roll, EQUALS_DELTA)));
+		assertThat("Euler Angles", o,  is(similarTo(yaw, pitch, roll)));
 	}
 
 }
