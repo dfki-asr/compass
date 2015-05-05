5b4b25aeba12bf674cac9a8fcf96e938d76fb9d9
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2015-05-05 15:29:46 +0200

    DeepCopy test for PrefabSets

--- compass-model/src/test/java/de/dfki/asr/compass/model/DeepCopyTest.java
+++ compass-model/src/test/java/de/dfki/asr/compass/model/DeepCopyTest.java
@@ -15,9 +15,21 @@ public class DeepCopyTest {
 	@Test
 	public void deepCopyPrefabSetShouldCreateCopy() throws IOException, ClassNotFoundException {
 		PrefabSet originalSet = new PrefabSet();
+		PrefabSet childSetA = new PrefabSet();
+		PrefabSet childSetB = new PrefabSet();
+		childSetA.setName("childA");
+		childSetB.setName("childB");
+		childSetA.setParent(originalSet);
+		childSetB.setParent(originalSet);
+		SceneNode prefabA = new SceneNode();
+		SceneNode prefabB = new SceneNode();
+		prefabA.setName("TestPrefabA");
+		prefabB.setName("TestPrefabB");
+		originalSet.addPrefab(prefabA);
+		originalSet.addPrefab(prefabB);
 		originalSet.setName("PrefabSetTest");
 		PrefabSet clone = (PrefabSet) originalSet.deepCopy();
-		assertEquals(originalSet.getName(), clone.getName());
+		assertTrue(prefabSetIsCopy(originalSet, clone));
 	}
 
 	@Test(enabled = false)
@@ -25,5 +37,44 @@ public class DeepCopyTest {
 		Project originalProject = new Project();
 		Project clone = (Project) originalProject.deepCopy();
 		assertTrue(clone.equals(originalProject));
+	}
+
+	private Boolean prefabSetIsCopy(final PrefabSet original, final PrefabSet copy) {
+		if (!original.getName().equals(copy.getName())) {
+			return false;
+		}
+		if (!prefabSetCopiedChildren(original, copy)) {
+			return false;
+		}
+		if (!prefabSetCopiedPrefabs(original, copy)) {
+			return false;
+		}
+		return true;
+	}
+
+	private Boolean prefabSetCopiedChildren(final PrefabSet original, final PrefabSet copy) {
+		if (original.getChildren().size() != copy.getChildren().size()) {
+			return false;
+		}
+		for (PrefabSet child: original.getChildren()) {
+			int childIndex = original.getChildren().indexOf(child);
+			if (!(copy.getChildren().get(childIndex).getName().equals(child.getName()))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private Boolean prefabSetCopiedPrefabs(final PrefabSet original, final PrefabSet copy) {
+		if (original.getPrefabs().size() != copy.getPrefabs().size()) {
+			return false;
+		}
+		for (SceneNode prefab: original.getPrefabs()) {
+			int prefabIndex = original.getPrefabs().indexOf(prefab);
+			if (!(copy.getPrefabs().get(prefabIndex).getName().equals(prefab.getName()))) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
