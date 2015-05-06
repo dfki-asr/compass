4179fd41c58251f78cb5a42d8dbba9e4d9d7a795
Daniel Spieldenner <daniel.spieldenner@dfki.de>
2015-05-06 13:46:45 +0200

    Tests for copying components in general, correct copying of preview images, clearing ids

--- compass-model/src/test/java/de/dfki/asr/compass/model/SceneNodeDeepCopyTest.java
+++ compass-model/src/test/java/de/dfki/asr/compass/model/SceneNodeDeepCopyTest.java
@@ -11,6 +11,7 @@ import de.dfki.asr.compass.model.components.PreviewImage;
 import de.dfki.asr.compass.model.components.RenderGeometry;
 import de.dfki.asr.compass.model.resource.Image;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
@@ -25,6 +26,18 @@ public class SceneNodeDeepCopyTest {
 	}
 
 	@Test
+	public void deepCopySceneNodesShouldClearIds() throws IOException, ClassNotFoundException {
+		SceneNode originalSceneNode = initializeSceneNode();
+		SceneNode copiedSceneNode = (SceneNode) originalSceneNode.deepCopy();
+		assertEquals(copiedSceneNode.getId(), 0);
+		for (SceneNode c: copiedSceneNode.getChildren()) {
+			assertEquals(c.getId(), 0);
+		}
+		for (SceneNodeComponent c: copiedSceneNode.getComponents()) {
+			assertEquals(c.getId(), 0);
+		}
+	}
+	@Test
 	public void deepCopySceneNodeShouldCopyChildNodes() throws IOException, ClassNotFoundException {
 		Boolean copiedChildren = true;
 		SceneNode originalSceneNode = initializeSceneNode();
@@ -38,6 +51,47 @@ public class SceneNodeDeepCopyTest {
 		assertTrue(copiedChildren);
 	}
 
+	@Test
+	public void deepCopySceneNodeShouldCopyTransform() throws IOException, ClassNotFoundException {
+		SceneNode originalSceneNode = initializeSceneNode();
+		SceneNode copiedSceneNode = (SceneNode) originalSceneNode.deepCopy();
+		assertEquals(originalSceneNode.getLocalTransform(), copiedSceneNode.getLocalTransform());
+	}
+
+	@Test
+	public void deepCopySceneNodeShouldCopyComponents() throws IOException, ClassNotFoundException {
+		Boolean copiedComponents = true;
+		SceneNode originalSceneNode = initializeSceneNode();
+		SceneNode copiedSceneNode = (SceneNode) originalSceneNode.deepCopy();
+		for (SceneNodeComponent c: originalSceneNode.getComponents()) {
+			int idx = originalSceneNode.getComponents().indexOf(c);
+			if (c.getClass().equals(PreviewImage.class)) {
+				Image originalImage = ((PreviewImage) c).getImage();
+				Image copyImage = ( (PreviewImage) copiedSceneNode.getComponents().get(idx)).getImage();
+				if (!(copiedPreview(originalImage, copyImage))) {
+					copiedComponents = false;
+					break;
+				}
+			}
+			if (c.getClass() != copiedSceneNode.getComponents().get(idx).getClass()) {
+				copiedComponents = false;
+			}
+		}
+		assertTrue(copiedComponents);
+	}
+
+	private Boolean copiedPreview(final Image original, final Image copy) {
+		if (!(original.getName().equals(copy.getName()))) {
+			return false;
+		}
+		if (!(original.getMimeType().equals(copy.getMimeType()))) {
+			return false;
+		}
+		if (!(Arrays.equals(original.getData(), copy.getData()))) {
+			return false;
+		}
+		return true;
+	}
 	private Boolean childrenAreCopies(final SceneNode original, final SceneNode copy) {
 		for (SceneNode originalChild: original.getChildren()) {
 			int idx = original.getChildren().indexOf(originalChild);
@@ -57,7 +111,9 @@ public class SceneNodeDeepCopyTest {
 		sceneNode.setLocalYaw(14.5);
 		sceneNode.setLocalRoll(14.5);
 		sceneNode.setLocalTranslation(new Vector3f(12f, -3.4f, 56.3f));
-		sceneNode.addComponent(new RenderGeometry());
+		RenderGeometry render = new RenderGeometry();
+		render.setId(7);
+		sceneNode.addComponent(render);
 		PreviewImage sceneNodePreview = generatePreview();
 		sceneNode.addComponent(sceneNodePreview);
 		addNodesTo(sceneNode);
@@ -71,6 +127,7 @@ public class SceneNodeDeepCopyTest {
 		image.setData(content);
 		PreviewImage preview = new PreviewImage();
 		preview.setImage(image);
+		preview.setId(6);
 		return preview;
 	}
 
