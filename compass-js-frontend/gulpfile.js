e66ed64032dd8b072717a2dc2c81b20f71e5c842
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-05-12 15:48:00 +0200

    gulp now includes browserify

--- compass-js-frontend/gulpfile.js
+++ compass-js-frontend/gulpfile.js
@@ -1,17 +1,44 @@
-var gulp = require("gulp");
+"use strict";
+
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
 var del = require("del");
 var pkg = require("./package.json");
 
-var srcFiles = [
-		"./src/main/webapp/index.html"
+var srcFolder = "./src/main/webapp/";
+var htmlFiles = [
+		srcFolder + "index.html"
 	];
-
 var destination = "./target/webapp";
 
-gulp.task("build", function(){
-	gulp.src(srcFiles)
+-gulp.task("build", function(){
+	bundle();
+	gulp.src(htmlFiles)
 			.pipe(gulp.dest(destination));
 });
+
+var browserifyOptions = {
+	entries: [srcFolder + "index.js"],
+	debug: true
+};
+var b = browserify(browserifyOptions);
+
+gulp.task("js", bundle); // so you can run `gulp js` to build the file
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest(destination));
+}
 
 gulp.task("clean", function(cb) {
 	del([destination], cb);
