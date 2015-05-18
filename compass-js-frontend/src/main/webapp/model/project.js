83b585e73088668f5b95d8ebdba5e445cbb913e6
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-05-18 11:32:45 +0200

    Add project model class

---
+++ compass-js-frontend/src/main/webapp/model/project.js
@@ -0,0 +1,40 @@
+/*
+ * This file is part of COMPASS. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this distribution.
+ * (Also available at http://www.apache.org/licenses/LICENSE-2.0.txt)
+ * You may not use this file except in compliance with the License.
+ */
+
+"use strict";
+
+var AmpersandModel = require("ampersand-model");
+
+var Project = AmpersandModel.extend({
+	props: {
+		name: {
+			type: "string",
+			required: true,
+			default: "Default Name"
+		},
+		scenarios: {
+			type: "array",
+			required: false,
+			default: function(){
+					return new Array();
+			}
+		},
+		prefabSet: "object"
+	},
+	session: {
+		selected: ["boolean", true, false]
+	},
+	ajaxConfig: {
+		headers: {
+			"Accept": "application/json"
+		}
+	},
+	extraProperties: 'ignore',
+	urlRoot: "http://localhost:8080/compass/resources/restv1/projects/"
+});
+
+module.exports = exports = Project;
