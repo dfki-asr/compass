ed095b88f85b694468df3056d3730db57277b096
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-05-18 11:32:57 +0200

    Add model collection class

---
+++ compass-js-frontend/src/main/webapp/collection/project-collection.js
@@ -0,0 +1,23 @@
+/*
+ * This file is part of COMPASS. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this distribution.
+ * (Also available at http://www.apache.org/licenses/LICENSE-2.0.txt)
+ * You may not use this file except in compliance with the License.
+ */
+
+"use strict";
+
+var AmpersandRestCollection = require("ampersand-rest-collection");
+var Project = require("./../model/project");
+
+var ProjectCollection = AmpersandRestCollection.extend({
+	model: Project,
+	ajaxConfig: {
+		headers: {
+			"Accept": "application/json"
+		}
+	},
+	url: "http://localhost:8080/compass/resources/restv1/projects/"
+});
+
+module.exports = exports = ProjectCollection;
