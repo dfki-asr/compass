aadaec3f3d8a75cfe3188b150a7b5d967ce9223d
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-05-20 14:52:53 +0200

    Add a app class as a starting point for everything

--- compass-js-frontend/src/main/webapp/app.js
+++ compass-js-frontend/src/main/webapp/app.js
@@ -7,3 +7,46 @@
 "use strict";
 
 window.$ = window.jQuery = require("jquery");
+
+var app = require("ampersand-app");
+
+app.extend({
+	init: function () {
+		console.log('Initializing the global App-Singleton.');
+		app.version = "2.0.1";
+		app.name = "COMPASS";
+		//app.router = new Router();
+		app.basePath = "/";
+		app.rootDomElement = document.getElementById("app");
+		$(document).ready(function () {
+			console.log('DOM is ready ... Initialize UI and Routing.');
+			app.initUI();
+			app.initRouting();
+		});
+		//helper function for easy navigating between pages (taken from AmpersandJS docs)
+		app.navigate = function(page) {
+			var url = (page.charAt(0) === '/') ? page.slice(1) : page;
+			if (url.indexOf('/') !== -1) {
+				url = url.match(/\/(.*?)$/)[1];
+			}
+			//this.router.history.navigate(url, {trigger: true});
+		};
+		app.initUI= function(){
+			console.log("Initalize UI ... Start with MainView");
+			//this.mainView = new MainView({
+			//	el: this.rootDomElement
+			//});
+		};
+		app.initRouting = function(){
+			console.log("Initalize routing ... navigate to /start");
+			app.navigate("/start");
+		};
+	}
+});
+
+module.exports = (function () {
+	console.log("Start Initialization ...");
+	app.init();
+	console.log("Shit is up and running ... COMPASS version " + app.version);
+}());
+
