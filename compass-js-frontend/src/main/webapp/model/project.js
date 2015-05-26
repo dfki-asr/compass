683b69715b5f4ab2d061f68c55517bfe69f34596
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-05-26 11:22:07 +0200

    Add some comment, why we need to wrap the ids

--- compass-js-frontend/src/main/webapp/model/project.js
+++ compass-js-frontend/src/main/webapp/model/project.js
@@ -27,6 +27,9 @@ var Project = AmpersandModel.extend({
 		selected: ["boolean", true, false]
 	},
 	parse: function(attrs){
+		//if the ids are just a plain array instead of being wrapped up as an array of {id: id} objects
+		//ampersand will not recognize them as the scenario ids
+		//as a result we need to wrap them properly before ampersand parses the attributes
 		var scenarios = attrs.scenarios;
 		for(var index in scenarios){
 			var id = scenarios[index];
