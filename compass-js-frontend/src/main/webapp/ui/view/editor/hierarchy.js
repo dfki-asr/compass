521fca906c4969a570a4f8a0af5f37faced5911c
Johannes Ebersold <johannes.ebersold@dfki.de>
2015-06-18 15:24:50 +0200

    Whitespace

--- compass-js-frontend/src/main/webapp/ui/view/editor/hierarchy.js
+++ compass-js-frontend/src/main/webapp/ui/view/editor/hierarchy.js
@@ -56,33 +56,33 @@ var HierarchyView = AmpersandView.extend({
 		this.tree = $tree.fancytree('getTree');
 		$tree.on('contextmenu', this.showContextMenu.bind(this));
 	},
-	preventMenuOnBackdrop: function() {
+	preventMenuOnBackdrop: function () {
 		var $backdrop = $('.basicContextContainer');
-		$backdrop.on('contextmenu', function() {
+		$backdrop.on('contextmenu', function () {
 			basicContext.close();
 			return false;
 		});
 	},
-	showContextMenu: function(event, data){
+	showContextMenu: function (event, data) {
 		var node = $.ui.fancytree.getNode(event);
 		this.selectCurrentNode(node);
 		var items = [
-			{ type: 'item', title: 'Add Node', icon: 'fa fa-plus-circle', fn: this.newChild.bind(this) },
-			{ type: 'item', title: 'Delete Node', icon: 'fa fa-trash-o', fn: this.deleteSelected.bind(this) }
+			{type: 'item', title: 'Add Node', icon: 'fa fa-plus-circle', fn: this.newChild.bind(this)},
+			{type: 'item', title: 'Delete Node', icon: 'fa fa-trash-o', fn: this.deleteSelected.bind(this)}
 		];
-		basicContext.show(items,event);
+		basicContext.show(items, event);
 		this.preventMenuOnBackdrop();
 		return false;
 	},
-	handleClickOnNode: function(event, data){
+	handleClickOnNode: function (event, data) {
 		var selectedNode = data.node;
 		this.selectCurrentNode(selectedNode);
 	},
-	selectCurrentNode: function(node) {
+	selectCurrentNode: function (node) {
 		var sceneNode = this.getSceneNodeByFancyNode(node);
 		this.parent.selectedNode = sceneNode;
 	},
-	updateSelectionDisplay: function() {
+	updateSelectionDisplay: function () {
 		if (!!this.parent.selectedNode) {
 			this.tree.activateKey(this.parent.selectedNode.cid);
 		} else {
@@ -90,29 +90,29 @@ var HierarchyView = AmpersandView.extend({
 			this.tree.activateKey(false);
 		}
 	},
-	createFancyTreeStructure: function(scenenode){
+	createFancyTreeStructure: function (scenenode) {
 		var fancyTree = [];
 		var self = this;
-		scenenode.children.each(function(child){
+		scenenode.children.each(function (child) {
 			fancyTree.push(self.createFancyTreeNode(child));
 		});
 		return fancyTree;
 	},
-	createFancyTreeNode: function(scenenode){
+	createFancyTreeNode: function (scenenode) {
 		var fancyNode = {};
 		fancyNode.title = scenenode.name;
 		fancyNode.key = scenenode.cid;
-		if(!scenenode.children.isEmpty()){
+		if (!scenenode.children.isEmpty()) {
 			fancyNode.folder = true;
 			fancyNode.children = this.createFancyTreeStructure(scenenode);
 		}
 		fancyNode.sceneNode = scenenode;
 		return fancyNode;
 	},
-	getSceneNodeByFancyNode: function(fancyNode){
+	getSceneNodeByFancyNode: function (fancyNode) {
 		return fancyNode.data.sceneNode;
 	},
-	getFancyNodeBySceneNode: function(sceneNode){
+	getFancyNodeBySceneNode: function (sceneNode) {
 		return this.tree.getNodeByKey(sceneNode.cid);
 	},
 	newChild: function () {
@@ -121,17 +121,17 @@ var HierarchyView = AmpersandView.extend({
 			sceneNode = this.parent.root;
 		}
 		var newNode = sceneNode.children.add({name: "New Node", parentNode: sceneNode});
-		newNode.save().then(function(){
+		newNode.save().then(function () {
 			// success
 			// we just expect that, so no need to tell the user anything.
-		}, function() {
+		}, function () {
 			// fail
 			_notify("danger", "Could not save your new node to the server.");
 		});
 		this.insertNodeIntoTree(newNode, sceneNode);
 		this.parent.selectedNode = newNode;
 	},
-	insertNodeIntoTree: function(sceneNode, parent) {
+	insertNodeIntoTree: function (sceneNode, parent) {
 		var index = sceneNode.collection.indexOf(sceneNode);
 		var fancyNode = this.createFancyTreeNode(sceneNode);
 		if (index === 0) {
